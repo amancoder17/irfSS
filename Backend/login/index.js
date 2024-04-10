2e3e79cd703b724b841768597d9bbf012ace8dcc
amancoder17 <aman.panwar.cs.2019@miet.ac.in>
2024-04-10 13:53:10 +0530

    vercel added

--- Backend/login/index.js
+++ Backend/login/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(
     {
-        origin:["hhttps://secret-santa-frontend-one.vercel.app/"],
+        origin:["hhttps://secret-santa-frontend-one.vercel.app"],
         methods:["POST","GET","UPDATE","DELETE","PATCH"],
         credentials:true
     }
